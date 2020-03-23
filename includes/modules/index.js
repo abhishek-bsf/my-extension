4760b8809a4740e90bf3aab6df96c20c2afa48cf
Abhishek <abhishek@localhost.localdomain>
2020-03-23 17:16:04 +0530

    initial commit

--- includes/modules/index.js
+++ includes/modules/index.js
@@ -1,3 +1,12 @@
 import HelloWorld from './HelloWorld/HelloWorld';
 
-export default [HelloWorld];
+// export default [HelloWorld];
+
+
+// Internal Dependencies
+import SimpleHeader from './SimpleHeader/SimpleHeader';
+
+export default [
+  SimpleHeader,
+  HelloWorld
+];
